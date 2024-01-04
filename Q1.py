08c67248ab9f8945848b3aa2b06bdab4b7bc74fc
angreno.18 <110455572+angreno@users.noreply.github.com>
2024-01-04 20:19:50 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,12 @@
+newList = [12, 35, 9, 56, 24]
+print(newList)
+def swapList(newList):
+    size = len(newList)
+
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+
+    return newList
+
+print(swapList(newList))
